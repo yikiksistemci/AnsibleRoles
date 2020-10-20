e7c9ea90af69be687a67ed3d312aa23f2984fea4
Yunus YASAR <Yunus.Yasar@sahibinden.com>
2020-10-20 23:48:41 +0300

    create cli usage template

---
+++ infra-studio.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#####################################################################
+############### Vagrant + Infra-as-CodE + Ansible ###################
+## Author: Yunus YASAR <yunus.yasar@pm.me>                         ##
+## Licence: GNU GPLv3                                              ##
+##                                                                 ##
+##                                                                 ##
+#####################################################################
+
+usage()
+{    
+    echo "infra-studio [-m <createvm,listvm> <HostName>] [-p <App Profile>] [-c <CPU>] [-r <RAM>] [-d <DISK>] [-add <IP Address>] "
+    echo " -m: Mode --> createvm,listvm "
+    echo " -p: App Profile --> Common,Graylog,Zabbix-Server,Docker,Docker-Compose vb."
+    echo " -l: list app profile"
+}
+
+# add config file 
+source infra-studio.conf
+
+if [ "$(id -u)" != "0" ]; then
+    echo "Only root may add a user to system"
+    exit 2
+fi
+
+
+
+
+
+while getopts "m:l:p:add:" ops; do
+
+    case "${ops}" in
+
+    l) echo "todo";;
+    m) echo "todo";;
+
+    *)
+      usage;;
+
+    esac
+
+done
+
+
+
+
+
+
